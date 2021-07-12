3adc52166c01b6178f3727963c2bcaea485a85bb
Jason Lutz <jason.m.lutz@gmail.com>
2021-07-12 16:20:02 -0700

    Build User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,53 @@
 #  id              :bigint           not null, primary key
 #  admin           :boolean          default(FALSE)
 #  password_digest :string           not null
+#  session_token   :string
 #  username        :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  has_many :facilities, foreign_key: :owner_id
+  has_many :reviews, foreign_key: :author_id, dependent: :destroy
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by username: username
+    if user && user.is_password?(password)
+      return user
+    else
+      return false
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@
 #  id              :bigint           not null, primary key
 #  admin           :boolean          default(FALSE)
 #  password_digest :string           not null
+#  session_token   :string
 #  username        :string           not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
