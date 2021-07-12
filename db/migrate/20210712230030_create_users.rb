5748c5f4e086dfef9a333df00f1eebefdd2882db
Jason Lutz <jason.m.lutz@gmail.com>
2021-07-12 16:09:16 -0700

    Perform User migration

--- db/migrate/20210712230030_create_users.rb
+++ db/migrate/20210712230030_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
-
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
+      
       t.timestamps
     end
   end
