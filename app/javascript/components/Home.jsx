38f9f7deea14f5e5e9681077b1243ac42cacd3f1
Jason Lutz <jason.m.lutz@gmail.com>
2021-07-13 15:24:06 -0700

    Add api/v1 controller

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import Users from "./Users";
+// import Header from "./Header";
+
+
+export default () => (
+    <div>
+      <h1>Users Index</h1>
+      // <Users />
+    </div>
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      post 'users/create'
+      delete 'users/:id', to: 'users#destroy'
+    end
+  end
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
