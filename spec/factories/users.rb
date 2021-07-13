0d0e4c6fe6d8a73406a50a9fc64f13b1c2816e7e
Jason Lutz <jason.m.lutz@gmail.com>
2021-07-13 15:00:45 -0700

    Update users and factories;

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,13 +5,14 @@
 #  id              :bigint           not null, primary key
 #  admin           :boolean          default(FALSE)
 #  password_digest :string
+#  session_token   :string           not null
 #  username        :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    password_digest { "MyString" }
+    username {"fb_user"}
+    password {"fb_password"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,59 @@
 #  id              :bigint           not null, primary key
 #  admin           :boolean          default(FALSE)
 #  password_digest :string
+#  session_token   :string           not null
 #  username        :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) do
+    FactoryBot.build(:user)
+    end
+
+    it { should validate_presence_of(:username)}
+    it { should validate_presence_of(:password_digest)}
+    it { should validate_presence_of(:session_token)}
+    it { should validate_uniqueness_of(:username)}
+    it { should validate_length_of(:password).is_at_least(6) }
+
+    describe '#is_password?' do
+      it 'identifies a correct password' do
+        expect(user.is_password?(user.password)).to be true
+      end
+      it 'identifies an incorrect password' do
+        expect(user.is_password?('incorrect_password')).to be false
+      end
+    end
+
+    describe '##find_by_credentials' do
+      it 'retrives the user object with correct credentials' do
+        user.save!
+        expect(User.find_by_credentials(user.username, user.password)).to eq(user)
+      end
+      it 'returns +nil+ if no such email exists' do
+        expect(User.find_by_credentials('invalidName', 'good_password')).to be false
+      end
+      it 'returns +nil+ if email exists but password is incorrect' do
+        expect(User.find_by_credentials(user.username, 'incorrect_password')).to be false
+      end
+    end
+
+    describe '#reset_session_token!' do
+      it 'sets a session token as a string' do
+        user.reset_session_token!
+        expect(user.session_token).to be_a(String)
+      end
+      it 'sets a session token to be a non-empty string' do
+        user.reset_session_token!
+        expect(user.session_token.length).to be > 0
+      end
+      it '*almost always* sets a new session token' do
+        @initial_session_token = user.session_token
+        user.reset_session_token!
+        expect(user.session_token).not_to eq(@initial_session_token)
+      end
+    end
 end
