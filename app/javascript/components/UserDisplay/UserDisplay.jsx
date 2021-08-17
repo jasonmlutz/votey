38008bbc682d97faaa52d94a8f121b6eb81a7514
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-17 16:24:09 -0700

    Refactor UserDisplay
    Props are explicitly listed as function arguments; it remains
    to add missing props validation.

--- app/javascript/components/UserDisplay/UserDisplay.jsx
+++ app/javascript/components/UserDisplay/UserDisplay.jsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { DATA } from "./UserFetchData"
 import { Link } from "react-router-dom";
-
-// const data = DATA;
 
 export default function UserDisplay({ user_id }) {
   const url = `/api/v1/users/${user_id}`;
@@ -52,42 +49,34 @@ export default function UserDisplay({ user_id }) {
   }
 }
 
-function UserHeader(props) {
-  return <div className="user-header">All about {props.username}!</div>;
+function UserHeader({ username }) {
+  return <div className="user-header">All about {username}!</div>;
 }
 
-function Activities(props) {
-  // props: polls, responses
+function Activities({ polls, responses }) {
   return (
     <>
-      <ActivityContainer type="polls" data={props.polls} />
-      <ActivityContainer type="responses" data={props.responses} />
+      <ActivityContainer type="polls" data={polls} />
+      <ActivityContainer type="responses" data={responses} />
     </>
   );
 }
 
-function ActivityContainer(props) {
+function ActivityContainer({ type, data }) {
   return (
     <div className="activity-container">
-      <ActivityHeader type={props.type} />
-      <ActivityList type={props.type} data={props.data} />
+      <ActivityHeader type={type} />
+      <ActivityList type={type} data={data} />
     </div>
   );
 }
 
-function ActivityHeader(props) {
-  const type = props.type;
+function ActivityHeader({ type }) {
   const message = type == "polls" ? "Polls Authored" : "Responses Submitted";
   return <div className="activity-header">{message}</div>;
 }
 
-function ActivityList(props) {
-  const type = props.type; // "polls" or "responses"
-  const data = props.data; // an array of poll or response objects
-  // console.log(type, data)
-  // return (
-  //   <h2>testing ActivityList for type = {type}</h2>
-  // )
+function ActivityList({ type, data }) {
   const activityListItems = data.map((item, index) => (
     <ActivityListItem key={index} item={item} type={type} />
   ));
@@ -99,12 +88,10 @@ function ActivityList(props) {
   }
 }
 
-function ActivityListItem(props) {
-  const item = props.item;
+function ActivityListItem({ item, type }) {
   // type expectations:
   // polls -> item is a Poll object
   // responses -> item is an array [response object, associated poll object]
-  const type = props.type;
   const path =
     type == "polls" ? `/polls/${item.id}` : `/responses/${item[0].id}`;
   const text = type == "polls" ? `${item.title}` : `${item[1].title}`;
