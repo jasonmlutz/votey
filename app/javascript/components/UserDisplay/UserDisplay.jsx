jasonmlutz/votey

commit 33701efb5574f3f8790a5d3fb1a606dc57c6f37e

Adjust UserDisplay state
See e7dfa5f; user fetch now sets data and mounted in one action,
hopefully preventing unnecessary api calls.

app/javascript/components/UserDisplay/UserDisplay.jsx
@@ -4,16 +4,13 @@ import { Link } from "react-router-dom";
 
 // const data = DATA;
 
-export default function UserDisplay(props) {
-  // props: user_id
-  const user_id = props.user_id
+export default function UserDisplay({user_id}) {
   const url = `/api/v1/users/${user_id}`
 
-  const [data, setData] = useState({});
-  const [mounted, setMountStatus] = useState(false);
+  const [data, setData] = useState({data: {}, mounted: false});
 
   useEffect(() => {
-    if (mounted == false) {
+    if (!data.mounted) {
       fetch(url)
         .then((data) => {
           if (data.ok) {
@@ -22,25 +19,24 @@ export default function UserDisplay(props) {
           throw new Error("network and/or server error")
         })
         .then((data) => {
-          setData(data);
-          setMountStatus(true);
+          setData({data: data, mounted: true});
         })
         .catch((err) => console.error("unknown error: " + err))
     }
   })
 
-  if (Object.keys(data).length) {
+  if (Object.keys(data.data).length) {
     return(
       <div className = "user-display">
-        <UserHeader username = { data.USER.username } />
+        <UserHeader username = { data.data.USER.username } />
         <Activities
-          polls = { data.POLLS}
-          responses = { data.RESPONSE_DATA }
+          polls = { data.data.POLLS}
+          responses = { data.data.RESPONSE_DATA }
         />
       </div>
     )
   } else {
-    if (mounted) {
+    if (data.mounted) {
       return <h2>No user info to display!</h2>
     } else {
       return <h2>Loading ...</h2>

commit 28bbcbb1413811298c9cb72294e8df2bfc14e803

Fix UserDisplay data state object
This commit fixes bugs unknowingly introduced in 33701ef, where
the new state object and related renaming was not done correctly
and not tested adequately.

Also renamed keys so that the data object has keys catalog and
mounted.

app/javascript/components/UserDisplay/UserDisplay.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function UserDisplay({ user_id }) {
   const url = `/api/v1/users/${user_id}`;
 
-  const [data, setData] = useState({ data: {}, mounted: false });
+  const [data, setData] = useState({ catalog: {}, mounted: false });
 
   useEffect(() => {
     if (!data.mounted) {
@@ -18,25 +18,25 @@ export default function UserDisplay({ user_id }) {
           }
           throw new Error("network and/or server error");
         })
-        .then((data) => {
-          setData({ data: data, mounted: true });
+        .then((catalog) => {
+          setData({ catalog: catalog, mounted: true });
         })
         .catch((err) => console.error("unknown error: " + err));
     }
   });
 
-  if (Object.keys(data.data).length) {
+  if (Object.keys(data.catalog).length) {
     return (
       <div className="user-display">
-        <UserHeader username={data.data.USER.username} />
+        <UserHeader username={data.catalog.USER.username} />
         <Activities
-          polls={data.data.POLLS}
-          responses={data.data.RESPONSE_DATA}
+          polls={data.catalog.POLLS}
+          responses={data.catalog.RESPONSE_DATA}
         />
       </div>
     );
   } else {
-    if (mounted) {
+    if (data.mounted) {
       setTimeout(function () {
         window.location.replace("/");
       }, 5000);