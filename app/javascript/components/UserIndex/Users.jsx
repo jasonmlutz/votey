96f99030297b05dc3f4d730a4d9a7a27eee08a11
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-23 12:30:40 -0700

    Add `isAdmin` prop to Users
    This commit adds the ability to call UsersIndex with (and without)
    admin privleges (though in practice, it will not be called at
    all unless by admin). Passing in the prop `isAdmin` with value `true`
    into Users will render UsersIndex with 4 columns: `id`, `username`,
    `admin`, and `delete`.
    
    It remains to add code to display something helpful in this final column
    (with a helpful action) and render this table only when
    viewing an admin's show page as that admin.

--- app/javascript/components/UserIndex/Users.jsx
+++ app/javascript/components/UserIndex/Users.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { UsersIndex } from "./UsersIndex";
 
-export function Users() {
-  return <UsersIndex keys={["id", "username", "admin"]} />;
+export function Users({ isAdmin }) {
+  var keys = ["id", "username", "admin"];
+  if (isAdmin) keys.push("delete");
+
+  return <UsersIndex keys={keys} />;
 }
+
+Users.defaultProps = {
+  isAdmin: false,
+};
