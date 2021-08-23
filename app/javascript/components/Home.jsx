b0c0b28c229116b35780242f8079ca9ab9d8cdba
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-23 13:49:29 -0700

    Remove UsersIndex from Home

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,5 @@
-import React, { useContext } from "react";
-import Users from "./UserIndex/Users";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import React from "react";
 
 export default function Home() {
-  const { currentUser } = useContext(CurrentUserContext);
-  const isAdmin = currentUser && currentUser.admin;
-  return <Users isAdmin={isAdmin} />;
+  return "nothing to see here!";
 }
