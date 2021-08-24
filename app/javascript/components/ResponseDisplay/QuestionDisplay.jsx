876b6ba08a14590460d7e1a9931a57263846b74b
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-24 10:27:30 -0700

    Add no responseOptions message to ResponseDisplay
    This commit continues work on #40, adding error handling to the
    ResponseDisplay components when a question has no response options.
    The strategy used here follow that used in 2c8d9e8.

--- app/javascript/components/ResponseDisplay/QuestionDisplay.jsx
+++ app/javascript/components/ResponseDisplay/QuestionDisplay.jsx
@@ -5,7 +5,8 @@ import ResponseOptionsContainer from "./ResponseOptionsContainer";
 export default function QuestionDisplay({ question, response_options }) {
   const title = question.title;
   const required = question.required;
-  const displayTitle = title + (required ? " *required*" : "");
+  const displayTitle =
+    title + (required && response_options.length ? " *required*" : "");
 
   const { answers } = useContext(AnswerContext);
   const question_id = question.id;
@@ -20,13 +21,22 @@ export default function QuestionDisplay({ question, response_options }) {
   // answers[question.id] = the answer object associated with that question
   // answer[question.id].id = the id of the associated response_option
 
-  return (
-    <li className="question-display-li">
-      <div className="question-title">{displayTitle}</div>
+  var responseOptionsInfo;
+  if (response_options.length) {
+    responseOptionsInfo = (
       <ResponseOptionsContainer
         response_options={response_options}
         selected_response_option_id={selected_response_option_id}
       />
+    );
+  } else {
+    responseOptionsInfo = <div>{"No response options to display!"}</div>;
+  }
+
+  return (
+    <li className="question-display-li">
+      <div className="question-title">{displayTitle}</div>
+      {responseOptionsInfo}
     </li>
   );
 }
