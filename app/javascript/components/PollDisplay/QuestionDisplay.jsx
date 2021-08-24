a9d184823aea22c1f9b3fdb72bc897055bb6e202
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-24 09:47:42 -0700

    Add empty response option logic to QuestionDisplay
    This commit includes logic to detect whether a question object
    has no response options and, in this case, whether the quesiton
    is required. If so, it display a message. Otherwise,
    ResponseOptionsContainer is rendered as usual.
    
    It remains to adjust the unanswered required question validation
    in the case when a required question has no response options, and
    is thus un-answerable.

--- app/javascript/components/PollDisplay/QuestionDisplay.jsx
+++ app/javascript/components/PollDisplay/QuestionDisplay.jsx
@@ -3,15 +3,28 @@ import ResponseOptionsContainer from "./ResponseOptionsContainer";
 
 export default function QuestionDisplay({ question, responseOptions }) {
   const title = question.title;
-  const required = question.required;
-  const displayTitle = title + (required ? " *required*" : "");
+  const isRequired = question.required;
+  const displayTitle = title + (isRequired ? " *required*" : "");
+
+  var responseOptionsInfo;
+  if (responseOptions.length) {
+    responseOptionsInfo = (
+      <ResponseOptionsContainer responseOptions={responseOptions} />
+    );
+  } else {
+    var noResponseOptionsText = "No response options to display!";
+    noResponseOptionsText += isRequired
+      ? " Moreover, this question is required!"
+      : null;
+    responseOptionsInfo = <div>{noResponseOptionsText}</div>;
+  }
 
   return (
     <li className="question-display-li">
       <div className="question-title" id={"question-title-" + question.id}>
         {displayTitle}
       </div>
-      <ResponseOptionsContainer responseOptions={responseOptions} />
+      {responseOptionsInfo}
     </li>
   );
 }
