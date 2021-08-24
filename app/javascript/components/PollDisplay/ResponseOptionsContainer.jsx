4414066a2e5d9c84d12dcdc8a8c7225c518b0022
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-24 08:44:46 -0700

    Add length 0 handling to ResponseOptionsContainer
    This commit adds a displayed message to the PollDispaly component
    in the case that a question has no response options. Note that
    the api/v2/polls endpoint returns an empty array for the
    response options object associated with the given question id,
    so that there will not be an error of attempting to interact
    with a null (or undefined) object.
    
    It remains to adjust the required question completeness
    front-end validation (see 05a17e3), as this change allows a required
    question to be un-answerable.

--- app/javascript/components/PollDisplay/ResponseOptionsContainer.jsx
+++ app/javascript/components/PollDisplay/ResponseOptionsContainer.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ResponseOptionDisplay from "./ResponseOptionDisplay";
 
 export default function ResponseOptionsContainer({ responseOptions }) {
-  const responseOptionListItems = responseOptions.map(
-    (responseOption, index) => (
-      <ResponseOptionDisplay key={index} responseOption={responseOption} />
-    )
+  var responseOptionListItems = (
+    <div className="no-content-message">
+      {"There's no response options here. That's odd ...."}
+    </div>
   );
+  if (responseOptions.length) {
+    responseOptionListItems = responseOptions.map((responseOption, index) => (
+      <ResponseOptionDisplay key={index} responseOption={responseOption} />
+    ));
+  }
   return (
     <div className="response-options-container radio-container">
       {responseOptionListItems}
