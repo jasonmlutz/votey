38ffc89cf151c53e1ebe0f4264b385f453ae11a6
Jason Lutz <jason.m.lutz@gmail.com>
2021-08-24 09:32:41 -0700

    Remove empty response options display
    This commit reverses the length 0 response options handling
    in ResponseOptionsContainer (see 4414066) in preparation for
    housing functionally similar logic in the parent QuestionDisplay
    component. This is possible since the QuestionDisplay already
    has the response options as a prop, so there is no need to pass
    an empty prop down to ResponseOptionsContainer and perform
    the logic there.
    
    Moreover, we can then also determine (without creating an additional
    prop) whether the question is required. This will allow for the
    addition of logic to detect whether a required question is
    un-answerable (due to absence of response options).

--- app/javascript/components/PollDisplay/ResponseOptionsContainer.jsx
+++ app/javascript/components/PollDisplay/ResponseOptionsContainer.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import ResponseOptionDisplay from "./ResponseOptionDisplay";
 
 export default function ResponseOptionsContainer({ responseOptions }) {
-  var responseOptionListItems = (
-    <div className="no-content-message">
-      {"There's no response options here. That's odd ...."}
-    </div>
-  );
-  if (responseOptions.length) {
-    responseOptionListItems = responseOptions.map((responseOption, index) => (
+  const responseOptionListItems = responseOptions.map(
+    (responseOption, index) => (
       <ResponseOptionDisplay key={index} responseOption={responseOption} />
-    ));
-  }
+    )
+  );
+
   return (
     <div className="response-options-container radio-container">
       {responseOptionListItems}
